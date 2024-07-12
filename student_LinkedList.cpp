6458ed503ec21317558ebd69046a55aaf9e74ac5
MR <motteza1182@gamil.com>
2024-07-12 19:49:03 +0330

    Update student_LinkedList.cpp

--- student_LinkedList.cpp
+++ student_LinkedList.cpp
@@ -1,114 +1,87 @@
 #include <iostream>
-#include <unistd.h>
+# include <unistd.h>
 
 using namespace std;
 
-void loading (){
-	cout << "LOADING : ";
-	sleep (2);
-	for (int i = 0 ; i < 9 ; i ++)
-	{
-		cout<< ".";
-		usleep(50000);
-	}
-	sleep (2);
-	for (int i =1 ; i <11 ; i++)
-	{
-		cout << ".";
-		usleep (50000);
-	}
-	cout << endl ;
-	sleep(1);
-	cout << "100% \npackages loaded ...";
-	sleep (1);
-	cout << "\n PROGRAM IS READY";
-	sleep (2);
-	cout << endl << endl ;
-}
-//////////////////////////////////////////////////////////////////////////
 void animation(string text)
 {
     for (int i = 0; i < text.length(); i++)
     {
         if (text.length() < 18)
             if (text.length() < 5)
-                usleep(30000);
+                usleep(3000);
             else
-                usleep(90000);
+                usleep(9000);
         else
-            usleep(20000);
+            usleep(2000);
         cout << text[i] << flush;
     }
-    cout << endl ;
+    cout << endl;
 }
-////////////////////////////////////////////////////////////////////////
+
 struct node
-{ 
-    int info;
+{
+    string first_name;
+    string last_name;
+    string ID;
     struct node *next;
 };
-///////////////////////////////////////////////////////////////////////
-class LinkedList {
-    public:
-        node* start; 
-        node* make_node(int value);
-        void add_last(int value);
-        void add_first(int value);
-        void add_in(int counter, int value);
-        void delete_last();
-        void delete_first();
-        void delete_in(int counter);
-        void display();
-        LinkedList()
-        { 
-            start = NULL;
-        }
-};
-node *LinkedList::make_node(int value) {
-    struct node *temp;
-    temp = new (struct node);
-    temp->info = value;
-    temp->next = NULL;
-    return temp;
-}
-void LinkedList::delete_last() {
-    if (start == NULL)
-	{ 
-        animation("The List is Empty");
-        return;
-    }
-    else if (start->next == NULL)
+
+class LinkedList
+{
+public:
+    node *start;
+    node *make_node();
+    void add_first();
+    void add_in();
+    void add_last();
+    void delete_first();
+    void delete_in();
+    void delete_last();
+    void display();
+    void Search();
+    LinkedList()
     {
         start = NULL;
-        animation("The list is now Empty");
     }
-    else
-    {
-    struct node *s, *temp;
-    s = start; 
-    while (s->next->next != NULL)
-	{ 
-        s = s->next;
-    }
-    temp = s->next->next;
-    s->next = NULL;
-    delete temp;
-    }
+};
+
+node *LinkedList::make_node()
+{
+    struct node *temp;
+    temp = new (struct node);
+
+    animation("enter the name :");
+    cin >> temp->first_name;
+    animation("enter the last name :");
+    cin >> temp->last_name;
+    animation("inter the ID :");
+    cin >> temp->ID;
+    temp->next = NULL;
+
+    return temp;
 }
-void LinkedList::delete_first() {
+
+void LinkedList::delete_first()
+{
     if (start == NULL)
-	{ 
+    {
         animation("The List is Empty");
         return;
     }
     node *temp = start;
     start = start->next;
-    delete temp; 
+    delete temp;
 }
-void LinkedList::delete_in(int counter)
+
+void LinkedList::delete_in()
 {
+    int counter;
+    animation("Enter the student number:");
+    cin >> counter;
+
     if (start == NULL)
-	{ 
+    {
         animation("The List is Empty");
         return;
     }
@@ -120,8 +93,8 @@ void LinkedList::delete_in(int counter)
     else
     {
         struct node *s, *temp;
-        s = start; 
-        for(int i = 0 ; i<counter-3 ; i++)
+        s = start;
+        for (int i = 0; i < counter - 3 ; i++)
         {
             s = s->next;
         }
@@ -130,110 +103,228 @@ void LinkedList::delete_in(int counter)
         delete temp;
     }
 }
-void LinkedList::add_last(int value) { 
-    struct node *temp, *s;
-    temp = make_node(value);
-    if(start == NULL)
-	{ 
-        start = temp; 
-        start->next = NULL;
-        cout << value ;
-        animation(" Inserted at List");
-        return; 
-    }
-    s = start; 
-    while (s->next != NULL)
-	{ 
-        s = s->next;
-    } 
-    temp->next = NULL;
-    s->next = temp;
-    cout << value;
-    animation(" Inserted at List");
-}
-void LinkedList::add_first(int value)
+
+void LinkedList::delete_last()
 {
-    //TODO: This is just an easter egg, don't pay attention
-        if(value == 14)
-        {
-        	int i = 0 ;
-            while(1)
-            {
-                cout << "010101010101010110101010110110010101010" ;
-                i ++;
-                if (i == 100)
-                {
-                	cout << "A....?    ";
-                    cout << "khob hala aghaie shabany moshkeli nadari ?????????";
-				}
-            }
-        }
-    struct node *temp ;
-    temp = make_node(value);
+    if (start == NULL)
+    {
+        animation("The List is Empty");
+        return;
+    }
+    else if (start->next == NULL)
+    {
+        start = NULL;
+        animation("The list is now Empty");
+        return;
+    }
+
+    struct node *s, *temp;
+    s = start;
+    while (s->next->next != NULL)
+    {
+        s = s->next;
+    }
+    temp = s->next;
+    s->next = NULL;
+    delete temp;
+}
+
+void LinkedList::add_first()
+{
+    struct node *temp;
+    temp = make_node();
     if (start == NULL)
     {
         start = temp;
         start->next = NULL;
-        cout << value;
         animation(" Inserted at List");
         return;
     }
     temp->next = start;
     start = temp;
-    cout << value;
     animation(" Inserted at List");
 }
-void LinkedList::add_in(int counter, int value)
+
+void LinkedList::add_in()
 {
+    int counter;
+    animation("Enter the student number:");
+    cin >> counter;
+
     struct node *temp, *s;
-    temp = make_node(value);
-    if(start == NULL)
-	{
+    temp = make_node();
+    if (start == NULL)
+    {
         start = temp;
-        cout << value;
         animation(" Inserted at List");
         return;
     }
     s = start;
-    for (int i = 0 ; i < counter-2 ; i++)
+    for (int i = 0; i < counter - 2; i++)
     {
         s = s->next;
     }
     temp->next = s->next;
     s->next = temp;
-    cout << value;
-    animation(" Inserted at List"); 
+    animation(" Inserted at List");
 }
-void LinkedList::display() {
-    struct node *temp;
-    if (start == NULL) 
-	{ 
+
+void LinkedList::add_last()
+{
+    struct node *temp, *s;
+    temp = make_node();
+    if (start == NULL)
+    {
+        start = temp;
+        animation(" Inserted at List");
+        return;
+    }
+    s = start;
+    while (s->next != NULL)
+    {
+        s = s->next;
+    }
+    s->next = temp;
+    animation(" Inserted at List");
+}
+
+void LinkedList::display()
+{
+    if (start == NULL)
+    {
         animation("The List is Empty");
-        return; 
-    } 
-    temp = start; 
+        return;
+    }
+    struct node *s;
+    s = start;
     animation("Elements of list are: ");
-    while (temp != NULL)
-	{ 
-        cout << temp->info << "->";
-        temp = temp->next;
-    } 
+    while (s != NULL)
+    {
+        cout << s->first_name << "->" << s->last_name << "->" << s->ID << endl;
+        cout << "|" << endl;
+        cout << "v" << endl;
+        s = s->next;
+    }
     animation("NULL");
 }
-///////////////////////////////////////////////////////////////////////////
-int main() {
-    loading();
-    system("color 0A");
-    LinkedList test;
-    test.add_first("1");
-    test.add_last(100);
-    test.add_first(2);
-    test.add_in(50,2);
-	test.add_first(5);
-	test.add_first(9);
-    test.add_in(55,2);
-	test.display();
-    test.delete_in(2);
-    test.display();
-    return 0; 
+
+void LinkedList::Search()
+{
+    string Search_Content;
+    int counter = 1;
+    animation("Enter value from student:");
+    cin >> Search_Content;
+
+    struct node *s, *temp;
+    s = start;
+
+    if (s->first_name == Search_Content)
+    {
+        animation("Student number in the list:");
+        cout << counter << endl;
+        return;
+    }
+    else if (s->last_name == Search_Content)
+    {
+        animation("Student number in the list:");
+        cout << counter << endl;
+        return;
+    }
+    else if (s->ID == Search_Content)
+    {
+        animation("Student number in the list:");
+        cout << counter << endl;
+        return;
+    }
+
+    while (s->next != NULL)
+    {
+        s = s->next;
+        counter++;
+        if (s->first_name == Search_Content)
+        {
+            animation("Student number in the list:");
+            cout << counter << endl;
+            return;
+        }
+        else if (s->last_name == Search_Content)
+        {
+            animation("Student number in the list:");
+            cout << counter << endl;
+            return;
+        }
+        else if (s->ID == Search_Content)
+        {
+            animation("Student number in the list:");
+            cout << counter << endl;
+            return;
+        }
+    }
+    animation("not found value !!!");
+}
+
+int main()
+{
+    LinkedList Student_Linkedlist;
+
+    int operator_index;
+
+    animation("dorood");
+    animation("khob bia shoro konim :");
+    while (true)
+    {
+        animation("what to do?");
+        animation("1) Add the student to the first list");
+        animation("2) Add the student to the middle of the list");
+        animation("3) Add the student to the end of the list");
+        animation("4) Remove the student from the first list");
+        animation("5) Remove the student from the middle of the list");
+        animation("6) Remove the student from the end of the list");
+        animation("7) display ");
+        animation("8) Search");
+        animation("9) Exit");
+
+        cin >> operator_index;
+        switch (operator_index)
+        {
+        case 1:
+            Student_Linkedlist.add_first();
+            break;
+
+        case 2:
+            Student_Linkedlist.add_in();
+            break;
+
+        case 3:
+            Student_Linkedlist.add_last();
+            break;
+
+        case 4:
+            Student_Linkedlist.delete_first();
+            break;
+
+        case 5:
+            Student_Linkedlist.delete_in();
+            break;
+
+        case 6:
+            Student_Linkedlist.delete_last();
+            break;
+
+        case 7:
+            Student_Linkedlist.display();
+            break;
+
+        case 8:
+            Student_Linkedlist.Search();
+            break;
+
+        case 9:
+            return 0;
+
+        default:
+            continue;
+        }
+    }
+    return 0;
 }
